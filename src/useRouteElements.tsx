73bc3845731c06949c0ae2f46726f17c4c5719ba
Quy <lequy140305@gmail.com>
2026-04-08 09:57:49 +0700

    Cấu hình Route cho trang đăng nhập, đăng ký

---
+++ src/useRouteElements.tsx
@@ -0,0 +1,28 @@
+import { useRoutes } from 'react-router-dom'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+import RegisterLayout from './layouts/RegiserLayout'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+  ])
+
+  return routeElements
+}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
+})
